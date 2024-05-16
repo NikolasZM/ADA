582b23966c02bb156bd000d2f4e043ae8d105ef3
NikolasZM <101744161+NikolasZM@users.noreply.github.com>
2024-05-15 20:48:47 -0500

    Add files via upload

---
+++ 1erEjercicio.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int mal(int n) {
+	int val = 1;
+	for (int i{ 0 }; i < n; i++) {
+		val = val * 2;
+	}
+	return val;
+}
+
+int bien(int n, int x) {
+	if (n == 1) {
+		return x;
+	}
+	int aux = bien(n / x,x);
+	return aux*aux;
+}
+
+
+
+
+int main() {
+	cout << mal(8) << "\n";
+	cout << bien(8,2);
+}
